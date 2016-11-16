2c7016b5d3c92e48cee5ff73571a61d97100576a
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-16 21:36:48 +0400

    add PhantomJS karma launcher

--- karma.conf.js
+++ karma.conf.js
@@ -8,35 +8,36 @@ module.exports = function(config) {
       'node_modules/angular-ui-router/release/angular-ui-router.min.js',
       'node_modules/angular-mocks/angular-mocks.js',
       // 'node_modules/jquery/dist/jquery.min.js', {
-        // 'pattern': 'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
-        // 'watched': false,
-        // 'served': true,
-        // 'included': true
+      // 'pattern': 'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
+      // 'watched': false,
+      // 'served': true,
+      // 'included': true
       // }, {
-        // 'pattern': 'test/fixtures/json/**/*.json',
-        // 'watched': true,
-        // 'served': true,
-        // 'included': false
+      // 'pattern': 'test/fixtures/json/**/*.json',
+      // 'watched': true,
+      // 'served': true,
+      // 'included': false
       // }, {
-        // 'pattern': 'test/fixtures/views/**/*.html',
-        // 'watched': true,
-        // 'served': true,
-        // 'included': false
+      // 'pattern': 'test/fixtures/views/**/*.html',
+      // 'watched': true,
+      // 'served': true,
+      // 'included': false
       // },
       'app/modules/app.module.js',
       'app/**/*.js',
       'test/unit/**/*.spec.js'
     ],
-
     exclude: [],
-
     preprocessors: {},
     reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [
+      // 'Chrome'
+      'PhantomJS'
+    ],
     singleRun: false,
     concurrency: Infinity
   });
