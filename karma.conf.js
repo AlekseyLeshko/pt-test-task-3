e26efd64d81952531c4e5279503fe65475122a23
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-16 22:09:24 +0400

    add find polyfill

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,7 @@ module.exports = function(config) {
     basePath: '',
     frameworks: ['jasmine'],
     files: [
+      'test/polyfills/find.js',
       'node_modules/angular/angular.min.js',
       'node_modules/angular-translate/dist/angular-translate.min.js',
       'node_modules/angular-ui-router/release/angular-ui-router.min.js',
